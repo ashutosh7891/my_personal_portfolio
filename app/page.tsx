1b6a86b6d7806533971a9457f91de98f5567e8d0
Ashutosh Yadav <74668315+ashutosh7891@users.noreply.github.com>
2025-08-22 15:23:52 +0530

    checking changes

--- app/page.tsx
+++ app/page.tsx
@@ -191,7 +191,7 @@ export default function Portfolio() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.2 }}
             >
-              Ashutosh Yadav
+              Ashutosh
             </motion.h1>
 
             <motion.p
